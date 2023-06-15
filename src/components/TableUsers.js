46ad59d96e0a44835354d76116863056876761dc
minhhoangg1911 <minhhoangg1911@gmail.com>
2023-06-15 15:22:59 +0700

    'modal-edt-user'

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,23 +1,27 @@
-
 import { useEffect, useState } from "react";
 import { Table } from "react-bootstrap";
 import { fetchAllUser } from "../services/UserService";
-import ReactPaginate from 'react-paginate';
+import ReactPaginate from "react-paginate";
 import ModalAddNew from "./ModalAddNew";
+import ModalEditUser from "./ModalEditUser";
+
 const TableUsers = (props) => {
   const [listUsers, setListUsers] = useState([]);
   const [totalUsers, setTotalUsers] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
 
   const [isShowModalAddNew, setIsShowModalAddNew] = useState(false);
+  const [isShowModalEdit, setIsShowModalEdit] = useState(false);
+  const [dataUserEdit, setDataUserEdit] = useState({});
 
   const handleClose = () => {
     setIsShowModalAddNew(false);
+    setIsShowModalEdit(false);
   };
 
   const handleUpdateTable = (user) => {
-    setListUsers([user,...listUsers]);
-  }
+    setListUsers([user, ...listUsers]);
+  };
 
   useEffect(() => {
     // call apis
@@ -26,36 +30,38 @@ const TableUsers = (props) => {
 
   const getUsers = async (page) => {
     let res = await fetchAllUser(page);
-    
+
     if (res && res.data) {
       setTotalUsers(res.total);
       setListUsers(res.data);
       setTotalPages(res.total_pages);
     }
-
   };
 
-const handlePageClick = (event) => {
-  getUsers(+event.selected + 1);
-}
+  const handlePageClick = (event) => {
+    getUsers(+event.selected + 1);
+  };
 
+  const handleEditUser = (user) => {
+    setDataUserEdit(user);
+    setIsShowModalEdit(true);
+  };
   return (
     <>
-
-<div className="my-3 add-new">
-            <span>
-              {" "}
-              <b>List User:</b>{" "}
-            </span>
-            <button
-              className="btn btn-success"
-              onClick={() => {
-                setIsShowModalAddNew(true);
-              }}
-            >
-              Add new user
-            </button>
-          </div>
+      <div className="my-3 add-new">
+        <span>
+          {" "}
+          <b>List User:</b>{" "}
+        </span>
+        <button
+          className="btn btn-success"
+          onClick={() => {
+            setIsShowModalAddNew(true);
+          }}
+        >
+          Add new user
+        </button>
+      </div>
 
       <Table striped bordered hover size="sm">
         <thead>
@@ -64,22 +70,31 @@ const handlePageClick = (event) => {
             <th>Email</th>
             <th>First Name</th>
             <th>Last Name</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          {listUsers && listUsers.length > 0 &&
-          listUsers.map((item,index)=> {
-            return (
-              <tr key={`user-${index}`}>
-              <td>{item.id}</td>
-              <td>{item.email}</td>
-              <td>{item.first_name}</td>
-              <td>{item.last_name}</td>
-            </tr>
-            )
-          })
-          }
-       
+          {listUsers &&
+            listUsers.length > 0 &&
+            listUsers.map((item, index) => {
+              return (
+                <tr key={`user-${index}`}>
+                  <td>{item.id}</td>
+                  <td>{item.email}</td>
+                  <td>{item.first_name}</td>
+                  <td>{item.last_name}</td>
+                  <td>
+                    <button
+                      className="btn btn-warning mx-3"
+                      onClick={() => handleEditUser(item)}
+                    >
+                      Edit
+                    </button>
+                    <button className="btn btn-danger">Delete</button>
+                  </td>
+                </tr>
+              );
+            })}
         </tbody>
       </Table>
 
@@ -91,9 +106,6 @@ const handlePageClick = (event) => {
         pageCount={totalPages}
         previousLabel="< previous"
         renderOnZeroPageCount={null}
-
-        
-       
         pageClassName="page-item"
         pageLinkClassName="page-link"
         previousClassName="page-item"
@@ -106,9 +118,17 @@ const handlePageClick = (event) => {
         activeClassName="active"
       />
 
-       <ModalAddNew show={isShowModalAddNew} handleClose={handleClose}
-       handleUpdateTable={handleUpdateTable}
-       />
+      <ModalAddNew
+        show={isShowModalAddNew}
+        handleClose={handleClose}
+        handleUpdateTable={handleUpdateTable}
+      />
+
+      <ModalEditUser
+        show={isShowModalEdit}
+        dataUserEdit={dataUserEdit}
+        handleClose={handleClose}
+      />
     </>
   );
 };
